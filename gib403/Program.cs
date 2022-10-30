ca12d04d7fbb52fb0a7b64878b6c9a89c0b534e9
JanickSpielmann <56891695+JanickSpielmann@users.noreply.github.com>
2022-10-30 17:25:08 +0100

    Abschluss Aufgabe 9.5

--- gib403/Program.cs
+++ gib403/Program.cs
@@ -1,4 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
-Console.Read();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Aufgabe9_5());  
+        }
+
+        public static int Aufgabe9_5()
+        {
+            Console.WriteLine("Geben Sie nun die erste Zahl ein");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geben Sie nun die zweite Zahl ein");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            if (zahl1 < zahl2)
+            {
+                int i = zahl2;
+                zahl2 = zahl1;
+                zahl1 = i;
+            }
+
+            while (zahl2 != 0)
+            {
+                int rest = zahl1 % zahl2;
+                zahl1 = zahl2;
+                zahl2 = rest;
+            }
+            Console.Write("Der ggt ist: ");
+            return zahl1;
+
+        }
+    }
+
+
+
